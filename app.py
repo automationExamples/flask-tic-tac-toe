b0680ebd4bb23cc8d59a5f615462faaff77926da
automationExamples <playwrightgithub@gmail.com>
2024-03-10 18:03:21 -0700

    add delete game

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ api = Api(app, version='1.0', title='Tic Tac Toe API',
 
 # Namespaces
 game_ns = Namespace('game', description='game operations')
+player_ns = Namespace('player', description='player operations')
 
 api.add_namespace(game_ns)
+api.add_namespace(player_ns)
 
 # In-memory data storage
 active_games = []
@@ -26,18 +28,34 @@ class Games(Resource):
         """List all active games"""
         return active_games
     
-@game_ns.route('/newGame')
+@game_ns.route('/new_game')
 class NewGame(Resource):
-    @game_ns.doc('newGame')
+    @game_ns.doc('new_game')
     def post(self):
         """Create a new game"""
         last_idx = active_games[-1]['id'] if len(active_games) > 0 else -1
-        new_game = {'id': last_idx + 1, 'board': [[0, 0, 0], [0, 0, 0], [0, 0, 0]]}
+        new_game = get_new_board(last_idx + 1)
 
         active_games.append(new_game)
         return new_game
+    
+@game_ns.route('/remove_game/<int:game_id>')
+@game_ns.response(404, 'Game not found')
+@game_ns.param('game_id', 'Game id to delete')
+class NewGame(Resource):
+    @game_ns.doc('removeGame')
+    def delete(self, game_id):
+        """Remove a game"""
+        for index, game in enumerate(active_games):
+            if game['id'] == game_id:
+                active_games.pop(index)
+                return {"message": f"Game with id {game_id} deleted"}
+        api.abort(404, f"Game with ID {game_id} not found")
 
 
+
+def get_new_board(id):
+    return {'id': id, 'board': [[0, 0, 0], [0, 0, 0], [0, 0, 0]]}
 
 if __name__ == '__main__':
     app.run(debug=True)
